504a730870765819c8ebc3c7f154e4eab2955647
GamboaBrayan <118092973+GamboaBrayan@users.noreply.github.com>
2024-12-02 18:42:14 -0500

    Add files via upload

---
+++ Js/scripts.js
@@ -0,0 +1,20 @@
+
+function openNav(){
+    document.getElementById("mobile-menu").style.width = "100%";
+}
+
+function closeNav(){
+    document.getElementById("mobile-menu").style.width = "0%";
+    
+}
+
+document.getElementById("My-joven").addEventListener("click", function() {
+    alert("Jóvenes de 18-30 años con estilo de vida activo e interesados en aprender sobre nutrición equilibrada y hábitos saludables. Que buscan opciones rápidas y prácticas para sus comidas teniendo como objetivo dieta balanceada y mejorar el rendimiento físico. Además, ofrecemos planes de comidas, recetas saludables y consejos de nutrición.");
+});
+
+document.getElementById("My-profe").addEventListener("click", function() {
+    alert("Profesionales con conocimiento y experiencia en nutrición.Interesados en estar actualizados y colaborar con otros profesionales.Teniendo como objetivo promover alimentación saludable y bienestar.Con la necesidad de encontrar información científica, recursos educativos,herramientas de cálculo y comunicación con otros expertos.");
+});
+
+
+
